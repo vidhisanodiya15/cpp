1fc461e8f11bfd428821c999edac353e34140d18
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-06-11 18:07:52 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+//int age = 45;
+//if(age>18){
+//	cout<<"your an adult";
+//}
+//else{
+//	cout<<"your are not an adult";
+//}
+int marks=67;
+if(marks<25){
+	cout<<"Grade is F";
+	
+}if(marks>=25 && marks<=44){
+	cout<<"grade is E";
+}
+}
