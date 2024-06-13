25b83d39a53f5dcc068dff84550685b637f77cd4
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-06-13 18:16:39 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int arr[5];
+    arr[1]=7;
+    arr[2]=7;
+    arr[4]=1;
+	cout<<arr[2];
+	
+}
---
+++ fun.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	//int num1,num2
+	int num1=53;
+	int num2=75;
+	int minimum=min(num1,num2);
+	cout<<minimum;
+	
+}
