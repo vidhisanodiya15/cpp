b37b500378239027d134d076f1b901a61c80a624
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-06-14 17:11:01 +0530

    Add files via upload

---
+++ ref.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+	void DoSomething(string s){
+	
+	s[1]='t';
+	cout<<s<<endl;
+	
+}int main(){
+	string s = "vidhi";
+	DoSomething(s);
+	cout<<s<<endl;
+	return 0;
+}
+
