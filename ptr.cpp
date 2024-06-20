67b60762e7e8b629254458fa1119744a79a1893c
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-06-20 22:37:38 +0530

    Add files via upload

---
+++ ptr.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int main(){
+// 	int num=5;
+// 	cout<<num;
+// 	int *ptr= &num;
+// 	cout<<" address of num is "<< &num<<endl;
+// 	
+// 	double d = 43.5;
+// 	double *p2= &d;
+// 	cout<< " value is "<< p2 << endl;
+// 	cout<<" address is "<< p2 <<endl;
+// 	cout<< " size of integer is "<< sizeof(num) << endl;
+// 	cout<<" size of pointer is "<< sizeof(ptr) <<endl;
+
+//
+//int x = 43;
+//int *ptr= &x;
+//cout<< x <<endl;
+//cout<< "value is "<< &x<< endl;
+//cout<< "address is "<< &x<< endl;
+ char ch = 'jhg';
+ char *p = &ch;
+ cout<< ch <<endl;
+ }
