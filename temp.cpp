a5eb50f6a9402c6cb43553360967f9ec9a0e37bf
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-06-24 21:49:47 +0530

    Add files via upload

---
+++ temp.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int temp[10];
+	//cout<<sizeof(temp)<<endl;
+	int *ptr=&temp[0];
+	cout<<sizeof(ptr)<<endl;
+	//cout<<sizeof(*ptr)<<endl;
+	
+}
