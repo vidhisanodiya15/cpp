a4aff9d47b7134bb691abcef6089c25401fba1ae
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-07-01 21:38:26 +0530

    Add files via upload

---
+++ op.cpp
@@ -0,0 +1,26 @@
+// CPP Program to demonstrate the Binary Operators
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 8, b = 3;
+
+    // Addition operator
+    cout << "a + b = " << (a + b) << endl;
+  
+    // Subtraction operator
+    cout << "a - b = " << (a - b) << endl;
+  
+    // Multiplication operator
+    cout << "a * b = " << (a * b) << endl;
+  
+    // Division operator
+    cout << "a / b = " << (a / b) << endl;
+  
+    // Modulo operator
+    cout << "a % b = " << (a % b) << endl;
+
+    return 0;
+}
+
