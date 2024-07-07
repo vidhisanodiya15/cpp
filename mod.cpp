22ba1eb406696b213272ad57dba6f2e646a91665
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-07-07 21:57:51 +0530

    Add files via upload

---
+++ mod.cpp
@@ -0,0 +1,18 @@
+// C++ program to demonstrate 
+// the signed modifier
+#include <iostream>
+using namespace std;
+
+// Driver Code
+int main()
+{
+	cout << "Size of signed int : " << 
+			sizeof(signed int) << 
+			" bytes" << endl;
+	cout << "Size of signed char : " << 
+			sizeof(signed char) << 
+			" bytes" << endl;
+
+	return 0;
+}
+
