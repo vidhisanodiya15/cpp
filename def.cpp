7ba2d0d7ff795a454baddad329594eb6945e088e
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-07-08 23:20:47 +0530

    Add files via upload

---
+++ def.cpp
@@ -0,0 +1,34 @@
+// C++ program to demonstrate 
+// Class 
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+class Geeks { 
+	// Access specifier 
+public: 
+	// Data Members 
+	string geekname; 
+
+	// Member Functions() 
+	void printname() 
+	{ 
+		cout << "Geekname is: " << geekname; 
+	} 
+}; 
+
+int main() 
+{ 
+
+	// Declare an object of class geeks 
+	Geeks obj1; 
+
+	// accessing data member 
+	obj1.geekname = "GeeksForGeeks"; 
+
+	// accessing member function 
+	obj1.printname(); 
+
+	return 0; 
+}
+
