30a939b1f5f103da2544e0cc05bd3faba4329590
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-07-10 18:51:24 +0530

    Add files via upload

---
+++ long mod.cpp
@@ -0,0 +1,17 @@
+// C++ program to demonstrate 
+// the long modifier
+#include <iostream>
+using namespace std;
+
+// Driver Code
+int main()
+{
+	cout << "Size of long int : " << 
+			sizeof(long int) << 
+			" bytes" << endl;
+	cout << "Size of long double : " << 
+			sizeof(long double) << 
+			" bytes" << endl;
+	return 0;
+}
+
