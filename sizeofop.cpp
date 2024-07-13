2826f6938a23de5ded27348c89b368d780a3bc7c
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-07-13 20:35:56 +0530

    Add files via upload

---
+++ sizeofop.cpp
@@ -0,0 +1,21 @@
+// C++ program to implement sizeof 
+// to determine the number of bytes 
+// taken by different data types 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// Driver code 
+int main() 
+{ 
+cout << "No of Bytes taken up by char is " << 
+		sizeof(char) << endl; 
+cout << "No of Bytes taken up by int is " << 
+		sizeof(int) << endl; 
+cout << "No of Bytes taken up by float is " << 
+		sizeof(float) << endl; 
+cout << "No of Bytes taken up by double is " << 
+		sizeof(double) << endl; 
+cout << "No of Bytes taken up by long is " << 
+		sizeof(long) << endl; 
+}
+
