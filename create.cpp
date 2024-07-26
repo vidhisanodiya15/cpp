b07e408e4424481f87739af46101e8a89b1fbb26
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-07-26 21:52:45 +0530

    Add files via upload

---
+++ create.cpp
@@ -0,0 +1,21 @@
+// C++ program to demonstrate
+// array of strings using
+// pointers character array
+#include <iostream>
+
+using namespace std;
+
+// Driver code
+int main()
+{
+    // Initialize array of pointer
+    const char* colour[4]
+        = { "Blue", "Red", "Orange", "Yellow" };
+
+    // Printing Strings stored in 2D array
+    for (int i = 0; i < 4; i++)
+        cout << colour[i] << "\n";
+
+    return 0;
+}
+
