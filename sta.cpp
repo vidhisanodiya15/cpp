d91f28be97b0f6ace5aa4a1f5355a195a3be955c
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-07-29 21:52:05 +0530

    Add files via upload

---
+++ sta.cpp
@@ -0,0 +1,29 @@
+// C++ program to execute all 5 
+// arithmetic function 
+#include <iostream> 
+using namespace std; 
+
+int main() 
+{ 
+	int GFG1, GFG2; 
+	GFG1 = 10; 
+	GFG2 = 3; 
+
+	// printing the sum of GFG1 and GFG2 
+	cout<< "GFG1 + GFG2= " << (GFG1 + GFG2) << endl; 
+
+	// printing the difference of GFG1 and GFG2 
+	cout << "GFG1 - GFG2 = " << (GFG1 - GFG2) << endl; 
+
+	// printing the product of GFG1 and GFG2 
+	cout << "GFG1 * GFG2 = " << (GFG1 * GFG2) << endl; 
+
+	// printing the division of GFG1 by GFG2 
+	cout << "GFG1 / GFG2 = " << (GFG1 / GFG2) << endl; 
+
+	// printing the modulo of GFG1 by GFG2 
+	cout << "GFG1 % GFG2 = " << (GFG1 % GFG2) << endl; 
+
+	return 0; 
+}
+
