9ea1c294308d5f1ca0485cf0903d365dc28cdbf3
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-07-30 21:37:36 +0530

    Add files via upload

---
+++ vfvg.cpp
@@ -0,0 +1,13 @@
+// Data Members
+int roll;
+int age;
+int marks;
+	
+// Member Functions
+void printDetails()
+{
+	cout<<"Roll = "<<roll<<"\n";
+	cout<<"Age = "<<age<<"\n";
+	cout<<"Marks = "<<marks;
+}
+
