910a91b8a1c013e769418b0ee9c3d90fe3ac8be8
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-09-19 23:43:48 +0530

    Create Directives.cpp

---
+++ Directives.cpp
@@ -0,0 +1,18 @@
+// preprocessor directives
+#include <bits/stdc++.h>
+ 
+#define min(a, b) ((a < b) ? a : b)
+ 
+using namespace std;
+ 
+// Driver code
+int main()
+{
+    int a = 2, b = 4;
+ 
+    // Find the minimum of a and b
+    cout << "Minimum of both is: "
+         << min(a, b);
+ 
+    return 0;
+}
